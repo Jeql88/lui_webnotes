fe87040815dd754c26fc745311952dfbd538ebc5
Jeql88 <160448567+Jeql88@users.noreply.github.com>
2024-03-06 12:23:44 +0800

    Delete java.js

--- java.js
+++
@@ -1,5 +0,0 @@
-function computeVal(){
-    var add = parseFloat(document.getElementById("num1").value) + (parseFloat(document.getElementById("num1").value) * parseFloat(document.getElementById("quality").value))
-    document.getElementById("value").innerHTML = (add /parseFloat(document.getElementById("people").value)).toFixed(2)
-
-}
